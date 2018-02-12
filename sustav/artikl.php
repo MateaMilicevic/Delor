71b90dc20c39a83cb953b9fd78322f627180328a
Leoprimorac <leoprimorac96@gmail.com>
2018-02-12 14:09:54 +0100

    Obrisano

--- sustav/artikl.php
+++ sustav/artikl.php
@@ -1,5 +1,35 @@
 
- <!DOCTYPE>
+<?php
+
+require 'db.php';
+require 'session.php';
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if(isset($_POST['potvrda'])) {
+		
+		$naziv = $_POST['naziv'];
+		$neto_kolicina = $_POST['neto_kolicina'];
+		$cijena = $_POST['cijena'];
+		$dostupnost = $_POST['dostupnost'];
+		$_SESSION['logged_in'] = true;
+
+		
+
+		
+			$sql = "INSERT INTO artikal (naziv, neto_kolicina, cijena, dostupnost) "
+			. "VALUES ('$naziv','$neto_kolicina','$cijena', '$dostupnost')";
+			if ($mysqli->query($sql)){
+				header("location: profil.php");
+			}
+	}	
+
+	}
+
+
+?>
+<!DOCTYPE>
 <html>
 <head>
 	<meta charset="utf-8">
