2a828b748425e9995c0927b5d11af94ac8fc15a8
MateaMilicevic <mateaaa.milicevic@gmail.com>
2018-02-12 16:18:43 +0100

    novo

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 		  </button>
 		  <div class="collapse navbar-collapse" id="navbarResponsive">
 		    <ul class="navbar-nav ml-auto">
-	          <li class="nav-item option"><a class="nav-link" href="">Početna</a></li>
-	          <li class="nav-item option"><a class="nav-link" href="">Narudžbe &amp; Dostava</a></li>
-	          <li class="nav-item option"><a class="nav-link" href="">Kontakt</a></li>
+	          <li class="nav-item option"><a class="nav-link" href="#home">Početna</a></li>
+	          <li class="nav-item option"><a class="nav-link" href="#about">Narudžbe &amp; Dostava</a></li>
+	          <li class="nav-item option"><a class="nav-link" href="#contact">Kontakt</a></li>
 	          <li class="nav-item option"><a class="nav-link" href="public/vizija.php">Vizija</a></li>
 	          <li class="nav-item option"><a class="nav-link" href="public/prijava/index.php">Prijava</a></li>
 	          <li class="nav-item option"><a class="nav-link" href="public/prijava/registracija.php">Registracija</a></li>
@@ -33,10 +33,38 @@
 
 		<div id="glavni" class="row">
 			<div id="tekst" class="col-12 col-md-8 d-flex flex-column justify-content-center align-items-center">
-			    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				<section id="home" class="row d-flex flex-column justify-content-center align-items-center">
+			    <p><br><br><br> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
 					sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+				</section>
+				<section id="about" class="row d-flex flex-column justify-content-center align-items-center">
+					<p> <br><br><br>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and 
+					I will give you a complete account of the system, and expound the actual teachings of the great explorer of the 
+					truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is 
+					pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely 
+					painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but
+					 because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial
+					  example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But
+					   who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences,
+					    or one who avoids a pain that produces no resultant pleasure?"</p>
+				</section>
+				<section id="contact" class="row d-flex flex-column justify-content-left align-items-center">
+				<br><br><br>
+				  <h2 class="text-left text-black">Kontaktirajte nas</h2><br>
+          			<div class="col-12 col-md-12 contact_info text-black">
+					  
+                      	  Telefon:<br>
+              			  +387 36 123 456<br><br>
+               			  Mobitel:<br>
+              			  +387 63 789 123<br><br>
+              			  Email:<br>
+              			  delor@gmail.com<br><br>
+           			 </div>
+
+				</section>
+				
 			</div>
 			<div id="reklame1" class="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
 			  	<div>
@@ -59,7 +87,7 @@
 
 		<div id="info" class="row">
 			<div class="text-center">
-				LEO LEO DIL DIL DIL
+				 
 			</div>
 		</div>
 	</div>
