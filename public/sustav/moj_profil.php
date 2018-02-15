3cece3651c1c17fda091b425b94319486b293afd
MateaMilicevic <mateaaa.milicevic@gmail.com>
2018-02-15 21:00:19 +0100

    malomojprofiluredjen

--- public/sustav/moj_profil.php
+++ public/sustav/moj_profil.php
@@ -56,7 +56,7 @@ session_start();
                 </div>
 			
                 <div class="col-9 boja2">
-				<nav id="myNavbar" class="navbar sticky-top navbar-toggleable-md navbar-light bg-faded">
+				<nav id="myNavbar1" class="navbar sticky-top navbar-toggleable-md navbar-light bg-faded">
 		  			<button class="navbar-toggler navbar-toggler-center" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		    			<span class="navbar-toggler-icon"></span>
 		  			</button>
